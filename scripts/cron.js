18afd649d780aaaa3f462e2f05cf14ebf0639414
oppai <hotsoup.h@gmail.com>
2015-06-26 18:41:30 +0900

    update cron.js

--- scripts/cron.js
+++ scripts/cron.js
@@ -3,7 +3,9 @@ var Cron = require('cron').CronJob;
 var Client = require('node-rest-client').Client;
 var Exec = require('child_process').exec;
 
-var SCHEDULE_URL = "http://192.168.11.200:8000/api/reserves.json";
+var HOST = "192.168.11.200";
+var SSH_CMD = "ssh kodam@"+HOST+" -A -i /Users/murayamahiroaki/.ssh/kapi_rsa";
+var SCHEDULE_URL = "http://" + HOST + ":8000/api/reserves.json";
 var MORNING_CRON_FORMAT = "0 30 6 * * *";
 var NIGHT_CRON_FORMAT   = "0 50 21 * * *";
 var password = '';
@@ -18,28 +20,42 @@ if(DEBUG){
 }
 
 module.exports = function(robot){
+    var wakeup = function(){
+        wakeonlan(function(){
+            robot.send({ room: ROOM }, "anikapiをつけたよ！");
+        }, function(){
+            robot.send({ room: ROOM }, "anikapiを付けられなかったよ…");
+        });
+    };
+    var goodnight = function(){
+        shutdown(function(){
+            robot.send({ room: ROOM }, "anikapiを止めたよ！");
+        }, function(){
+            robot.send({ room: ROOM }, "anikapiを止められなかったよ…");
+        });
+    };
+
     var m_job = new Cron(MORNING_CRON_FORMAT, function(){
         postAnimeList(function(text){
             robot.send({ room: ROOM }, "今晩の予約済みのアニメは");
             robot.send({ room: ROOM }, text);
             robot.send({ room: ROOM }, "だよ！ <!channel>");
-
-            shutdown(function(){
-                robot.send({ room: ROOM }, "anikapiを止めたよ！");
-            }, function(){
-                robot.send({ room: ROOM }, "anikapiを止められなかったよ…");
-            });
+            goodnight();
         });
     }, null, true, "Asia/Tokyo");
 
     var n_job = new Cron(NIGHT_CRON_FORMAT, function(){
-            robot.send({ room: ROOM }, "アニメサーバをつけるよ！");
-            wakeonlan(function(){
-                robot.send({ room: ROOM }, "anikapiをつけたよ！");
-            }, function(){
-                robot.send({ room: ROOM }, "anikapiを付けられなかったよ…");
-            });
+        robot.send({ room: ROOM }, "アニメサーバをつけるよ！");
+        wakeup();
     }, null, true, "Asia/Tokyo");
+
+    robot.respond(/おはよう/i,function(msg){
+        wakeup();
+    });
+    robot.respond(/おやすみ/i,function(msg){
+        goodnight();
+    });
+
 };
 
 var postAnimeList = function(callback){
@@ -62,10 +78,11 @@ var formatedDateString = function(date) {
 }
 
 var shutdown = function(sc,ec){
-    var child = Exec("echo '"+password+"' | ssh -A kapi sudo -S shutdown -h now",function (error, stdout, stderr) {
+    var child = Exec("echo '"+password+"' | "+SSH_CMD+" sudo -S shutdown -h now",function (error, stdout, stderr) {
         if(!error){
             sc();
         }else{
+            console.log(error);
             ec();
         }
     });
@@ -75,6 +92,7 @@ var wakeonlan = function(sc,ec){
         if(!error){
             sc();
         }else{
+            console.log(error);
             ec();
         }
     });

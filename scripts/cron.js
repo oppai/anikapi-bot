6cacbf978cce57cd9bbd75fcd0aa7bdcad766630
oppai <hotsoup.h@gmail.com>
2015-10-12 11:13:54 +0900

    Fix username and rsa_path

--- scripts/cron.js
+++ scripts/cron.js
@@ -3,8 +3,10 @@ var Cron = require('cron').CronJob;
 var Client = require('node-rest-client').Client;
 var Exec = require('child_process').exec;
 
+var USER = ""
+var RSA_PATH = ""
 var HOST = "192.168.11.200";
-var SSH_CMD = "ssh -oStrictHostKeyChecking=no kodam@"+HOST+" -A -i /Users/murayamahiroaki/.ssh/kapi_rsa";
+var SSH_CMD = "ssh -oStrictHostKeyChecking=no "+USER+"@"+HOST+" -A -i "+RSA_PATH;
 var SCHEDULE_URL = "http://" + HOST + ":8000/api/reserves.json";
 var MORNING_CRON_FORMAT = "0 30 6 * * *";
 var NIGHT_CRON_FORMAT   = "0 50 21 * * *";
